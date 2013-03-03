a779a2c841016c26737f30c28c7f374560254f7c
Gautier Nilhcem <nilhcem@gmail.com>
2013-03-03 14:36:34 +0100

    Code review for SMTP Authentication handler

--- src/main/java/com/nilhcem/fakesmtp/server/SMTPAuthHandler.java
+++ src/main/java/com/nilhcem/fakesmtp/server/SMTPAuthHandler.java
@@ -3,51 +3,55 @@ package com.nilhcem.fakesmtp.server;
 import org.subethamail.smtp.AuthenticationHandler;
 import org.subethamail.smtp.RejectException;
 
-public class SMTPAuthHandler implements AuthenticationHandler {
+/**
+ * Simulates an authentication handler to allow capturing emails that are set up with login authentication.
+ *
+ * @author jasonpenny
+ * @since 1.2
+ */
+/*package*/ final class SMTPAuthHandler implements AuthenticationHandler {
+	private static final String USER_IDENTITY = "User";
+	private static final String PROMPT_USERNAME = "334 VXNlcm5hbWU6"; // VXNlcm5hbWU6 is base64 for "Username:"
+	private static final String PROMPT_PASSWORD = "334 UGFzc3dvcmQ6"; // UGFzc3dvcmQ6 is base64 for "Password:"
 
-    private int pass = 0;
+	private int pass = 0;
 
-    @Override
-    public String auth(String string) throws RejectException {
-        /**
-         * Initially called using an input string in the RFC2554 form: "AUTH <mechanism> [initial-response]". <br>
-         * This method must return text which will be delivered to the client, or null if the exchange
-         * has been completed successfully.  If a response is provided to the client, this continues
-         * the exchange - there will be another auth() call with the client's response.
-         * <p>
-         * Depending on the authentication mechanism, the handshaking process may require
-         * many request-response passes. This method will return <code>null</code> only when the authentication process is finished.
-         * <p>
-         * AuthenticationHandlers are associated with a single authentication exchange.  If the exchange is stopped
-         * (ie fails) and is restarted, a new AuthenticationHandler is created.  Upon successful authentication, your
-         * implementation of this object becomes part of the MessageContext.  Your MessageHandler may upcast your
-         * AuthenticationHandler to obtain further information, such as identity.
-         * <p>
-         * AuthenticationHandlers do not need to handle the "*" cancel response; this is handled by the framework.
-         *
-         * @return <code>null</code> if the authentication process is finished, otherwise a string to hand back to the client.
-         * @param clientInput The client's input, eg "AUTH PLAIN dGVzdAB0ZXN0ADEyMzQ="
-         * @throws org.subethamail.smtp.RejectException if authentication fails.
-         */
+	/**
+	 * Simulates an authentication process.
+	 * <ul>
+	 *   <li>first prompts for username;</li>
+	 *   <li>then, prompts for password;</li>
+	 *   <li>finally, returns {@code null} to finish the authentication process;</li>
+	 * </ul>
+	 * </p>
+	 *
+	 * @return <code>null</code> if the authentication process is finished, otherwise a string to hand back to the client.
+	 * @param clientInput The client's input, eg "AUTH PLAIN dGVzdAB0ZXN0ADEyMzQ="
+	 * @throws org.subethamail.smtp.RejectException if authentication fails.
+	 */
+	@Override
+	public String auth(String string) throws RejectException {
+		String prompt;
 
-        ++pass;
-        if (pass == 1)
-            return "334 VXNlcm5hbWU6";
-        if (pass == 2)
-            return "334 UGFzc3dvcmQ6";
+		if (++pass == 1) {
+			prompt = SMTPAuthHandler.PROMPT_USERNAME;
+		} else if (pass == 2) {
+			prompt = SMTPAuthHandler.PROMPT_PASSWORD;
+		} else {
+			pass = 0;
+			prompt = null;
+		}
+		return prompt;
+	}
 
-        return null;
-    }
-
-    @Override
-    public Object getIdentity() {
-        /**
-         * If the authentication process was successful, this returns the identity
-         * of the user.  The type defining the identity can vary depending on the
-         * authentication mechanism used, but typically this returns a String username.
-         * If authentication was not successful, the return value is undefined.
-         */
-        return "User";
-    }
-
+	/**
+	 * If the authentication process was successful, this returns the identity
+	 * of the user. The type defining the identity can vary depending on the
+	 * authentication mechanism used, but typically this returns a String username.
+	 * If authentication was not successful, the return value is undefined.
+	 */
+	@Override
+	public Object getIdentity() {
+		return SMTPAuthHandler.USER_IDENTITY;
+	}
 }
--- src/main/java/com/nilhcem/fakesmtp/server/SMTPAuthHandlerFactory.java
+++ src/main/java/com/nilhcem/fakesmtp/server/SMTPAuthHandlerFactory.java
@@ -5,18 +5,24 @@ import java.util.List;
 import org.subethamail.smtp.AuthenticationHandler;
 import org.subethamail.smtp.AuthenticationHandlerFactory;
 
-public class SMTPAuthHandlerFactory implements AuthenticationHandlerFactory {
+/**
+ * The factory interface for creating authentication handlers.
+ *
+ * @author jasonpenny
+ * @since 1.2
+ */
+/*package*/ final class SMTPAuthHandlerFactory implements AuthenticationHandlerFactory {
+	private static final String LOGIN_MECHANISM = "LOGIN";
 
-    @Override
-    public List<String> getAuthenticationMechanisms() {
-        List<String> result = new ArrayList<String>();
-        result.add("LOGIN");
-        return result;
-    }
+	@Override
+	public AuthenticationHandler create() {
+		return new SMTPAuthHandler();
+	}
 
-    @Override
-    public AuthenticationHandler create() {
-        return new SMTPAuthHandler();
-    }
-
+	@Override
+	public List<String> getAuthenticationMechanisms() {
+		List<String> result = new ArrayList<String>();
+		result.add(SMTPAuthHandlerFactory.LOGIN_MECHANISM);
+		return result;
+	}
 }
