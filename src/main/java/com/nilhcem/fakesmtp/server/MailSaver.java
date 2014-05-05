bee0380b9dc73168273fa79fd5f985ec425548bf
Nicolas Filotto <nfilotto@exoplatform.com>
2014-05-05 14:43:43 +0000

    Added the memory mode + improved the overall performances

--- src/main/java/com/nilhcem/fakesmtp/server/MailSaver.java
+++ src/main/java/com/nilhcem/fakesmtp/server/MailSaver.java
@@ -1,5 +1,15 @@
 package com.nilhcem.fakesmtp.server;
 
+import com.nilhcem.fakesmtp.core.ArgsHandler;
+import com.nilhcem.fakesmtp.core.Configuration;
+import com.nilhcem.fakesmtp.core.I18n;
+import com.nilhcem.fakesmtp.model.EmailModel;
+import com.nilhcem.fakesmtp.model.UIModel;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -15,15 +25,6 @@ import java.util.Observable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.nilhcem.fakesmtp.core.Configuration;
-import com.nilhcem.fakesmtp.core.I18n;
-import com.nilhcem.fakesmtp.model.EmailModel;
-import com.nilhcem.fakesmtp.model.UIModel;
-
 /**
  * Saves emails and notifies components so they can refresh their views with new data.
  *
@@ -32,7 +33,10 @@ import com.nilhcem.fakesmtp.model.UIModel;
  */
 public final class MailSaver extends Observable {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MailSaver.class);
-	private final Pattern subjectPattern = Pattern.compile("^Subject: (.*)$");
+	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	// This can be a static variable since it is Thread Safe
+	private static final Pattern SUBJECT_PATTERN = Pattern.compile("^Subject: (.*)$");
+
 	private final SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyhhmmssSSS");
 
 	/**
@@ -40,7 +44,7 @@ public final class MailSaver extends Observable {
 	 *
 	 * @param from the user who send the email.
 	 * @param to the recipient of the email.
-	 * @param data an inputstream object containing the email.
+	 * @param data an InputStream object containing the email.
 	 * @see com.nilhcem.fakesmtp.gui.MainPanel#addObservers to see which observers will be notified
 	 */
 	public void saveEmailAndNotify(String from, String to, InputStream data) {
@@ -61,16 +65,18 @@ public final class MailSaver extends Observable {
 			}
 		}
 
+		// We move everything that we can move outside the synchronized block to limit the impact
+		EmailModel model = new EmailModel();
+		model.setFrom(from);
+		model.setTo(to);
+		String mailContent = convertStreamToString(data);
+		model.setSubject(getSubjectFromStr(mailContent));
+		model.setEmailStr(mailContent);
+
 		synchronized (getLock()) {
-			String mailContent = convertStreamToString(data);
 			String filePath = saveEmailToFile(mailContent);
 
-			EmailModel model = new EmailModel();
 			model.setReceivedDate(new Date());
-			model.setFrom(from);
-			model.setTo(to);
-			model.setSubject(getSubjectFromStr(mailContent));
-			model.setEmailStr(mailContent);
 			model.setFilePath(filePath);
 
 			setChanged();
@@ -83,7 +89,8 @@ public final class MailSaver extends Observable {
 	 */
 	public void deleteEmails() {
 		Map<Integer, String> mails = UIModel.INSTANCE.getListMailsMap();
-
+		if (ArgsHandler.INSTANCE.memoryModeEnabled())
+			return;
 		for (String value : mails.values()) {
 			File file = new File(value);
 			if (file.exists()) {
@@ -118,8 +125,8 @@ public final class MailSaver extends Observable {
 	 * These 4 lines are SubEtha SMTP additional information.
 	 * </p>
 	 *
-	 * @param is the inputstream to be converted.
-	 * @return the converted string object, containing data from the inputstream passed in parameters.
+	 * @param is the InputStream to be converted.
+	 * @return the converted string object, containing data from the InputStream passed in parameters.
 	 */
 	private String convertStreamToString(InputStream is) {
 		final long lineNbToStartCopy = 4; // Do not copy the first 4 lines (received part)
@@ -131,7 +138,7 @@ public final class MailSaver extends Observable {
 		try {
 			while ((line = reader.readLine()) != null) {
 				if (++lineNb > lineNbToStartCopy) {
-					sb.append(line + System.getProperty("line.separator"));
+					sb.append(line).append(LINE_SEPARATOR);
 				}
 			}
 		} catch (IOException e) {
@@ -147,6 +154,8 @@ public final class MailSaver extends Observable {
 	 * @return the path of the created file.
 	 */
 	private String saveEmailToFile(String mailContent) {
+		if (ArgsHandler.INSTANCE.memoryModeEnabled())
+			return null;
 		String filePath = String.format("%s%s%s", UIModel.INSTANCE.getSavePath(), File.separator,
 				dateFormat.format(new Date()));
 
@@ -186,7 +195,7 @@ public final class MailSaver extends Observable {
 
 			String line;
 			while ((line = reader.readLine()) != null) {
-				 Matcher matcher = subjectPattern.matcher(line);
+				 Matcher matcher = SUBJECT_PATTERN.matcher(line);
 				 if (matcher.matches()) {
 					 return matcher.group(1);
 				 }
